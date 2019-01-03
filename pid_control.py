65b444f73b6fdd6375476aebf1c34cd1fd50f82b
LuciEN <lucien.liu@users.noreply.github.com>
2019-01-03 12:53:00 -0500

    updated

--- pid_control.py
+++ pid_control.py
@@ -1,63 +1,65 @@
 from math import sqrt
 
+epochLength_ = 200;
 
 class PID(object):
-    def __init__(self, Kp, Ki, Kd):
-        self.p_error = 0.0
-        self.i_error = 0.0
-        self.d_error = 0.0
+	def __init__(self, Kp, Ki, Kd):
+		self.p_error = 0.0
+		self.i_error = 0.0
+		self.d_error = 0.0
 
-        self.i_e_fabs = 0.0
+		self.i_e_fabs = 0.0
+		
+		self.Kp = Kp
+		self.Ki = Ki
+		self.Kd = Kd
 
-        self.Kp = Kp
-        self.Ki = Ki
-        self.Kd = Kd
+		self.counter_ = 0
+		self.epochCumulativeError_ = 0.0
+		self.previousEpochError_ = 0.0
+		self.needsTraining_ = True
+		self.currentEpochError_ = 0.0
+		self.errorThreshold_ = 0.005
+		self.learnRate_ = 0.01
 
-        self.counter_ = 0
-        self.epochLength_ = 200
-        self.epochCumulativeError_ = 0.0
-        self.previousEpochError_ = 0.0
-        self.needsTraining_ = True
-        self.currentEpochError_ = 0.0
-        self.errorThreshold_ = 0.005
-        self.learnRate_ = 0.01
+	def updateEpochError(self, cte):
+		self.i_e_fabs_ += abs(cte)
+		self.epochCumulativeError_ += (cte*cte)	
 
-    def updateEpochError(self, cte): #
-        self.i_e_fabs_ += abs(cte)
-        self.epochCumulativeError_ += (cte * cte)
+	def resetEpochError(self):
+		self.i_e_fabs_ = 0.0
+		self.epochCumulativeError_ = 0.0
 
-    def resetEpochError(self): #
-        self.i_e_fabs_ = 0.0
-        self.epochCumulativeError_ = 0.0
+	def evaluate(self):
+		if needsTraining_:
+			self.currentEpochError_ = sqrt(self.epochCumulativeError_ / self.epochLength_)
+			self.needsTraining_ = self.currentEpochError_ > self.errorThreshold_			
 
-    def evaluate(self): #
-        self.currentEpochError_ = sqrt(self.epochCumulativeError_ / self.epochLength_)
-        self.needsTraining_ = self.currentEpochError_ > self.errorThreshold_
-        return self.needsTraining_
+	def adjust(self, Kx, dx, dE):
+		if(Kx == 'p'):
+			partialDKx = self.Kp * dx * dE * self.learnRate_;
+			self.Kp -= partialDKx;
+		if(Kx == 'i'):
+			partialDKx = self.Ki * dx * dE * self.learnRate_;
+			self.Ki -= partialDKx;
+		if(Kx == 'd'):
+			partialDKx = self.Kd * dx * dE * self.learnRate_;
+			self.Kd -= partialDKx;
 
-    def adjust(self, Kx, dx, dE): #
-        if (Kx == 'p'):
-            partialDKx = self.Kp * dx * dE * self.learnRate_
-            self.Kp -= partialDKx
-        if (Kx == 'i'):
-            partialDKx = self.Ki * dx * dE * self.learnRate_
-            self.Ki -= partialDKx
-        if (Kx == 'd'):
-            partialDKx = self.Kd * dx * dE * self.learnRate_
-            self.Kd -= partialDKx
 
-    def backProp(self):
-        deltaError = self.previousEpochError_ - self.currentEpochError_
-        self.previousEpochError_ = self.currentEpochError_
-        self.adjust('p', -self.p_error, deltaError)
-        self.adjust('i', -self.i_e_fabs_, deltaError)
-        self.adjust('d', -self.d_error, deltaError)
+	def backProp(self):
+		deltaError = self.previousEpochError_ - self.currentEpochError_
+		self.previousEpochError_ = self.currentEpochError_
+		self.adjust('p', -self.p_error, deltaError)
+		self.adjust('i', -self.i_e_fabs_, deltaError)
+		self.adjust('d', -self.d_error, deltaError)
 
-    def UpdateError(self, cte): #
-        self.d_error = cte - self.p_error
-        self.p_error = cte
-        self.i_error += cte
-        self.updateEpochError(cte)
+	def UpdateError(self, cte):
+		self.d_error = cte - self.p_error
+		self.p_error = cte
+		self.i_error += cte
+		self.updateEpochError(cte)
 
-    def TotalError(self): #
-        return -self.Kp * self.p_error - self.Ki * self.i_error - self.Kd * self.d_error
+	def TotalError(self):
+		return -self.Kp * self.p_error - self.Ki * self.i_error - self.Kd * self.d_error
+
