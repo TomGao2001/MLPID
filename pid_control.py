87db48d0c2a6632e2c6bde035c3af3ef3e22877f
LuciEN <lucien.liu@users.noreply.github.com>
2019-04-30 10:16:36 -0400

    Update pid_control.py

--- pid_control.py
+++ pid_control.py
@@ -52,7 +52,8 @@ class PID(object):
 	def evaluate(self):
 		self.currentEpochError_ = sqrt(self.epochCumulativeError_ / self.epochLength_)/100
 		#self.updateSpeedCoeff()
-		self.needsTraining_ = self.currentEpochError_ > self.errorThreshold_			
+		#self.needsTraining_ = self.currentEpochError_ > self.errorThreshold_	
+		self.needsTraining_ = False		
 
 	def adjust(self, Kx, dx, dE):
 		if Kx == 'p':
